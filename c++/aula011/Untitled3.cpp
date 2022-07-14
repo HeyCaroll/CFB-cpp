1d75ecf3870cbaf5111eb0d8436237feeafafe1d
caroliny silva <caroliny.silv4@outlook.com>
2022-07-13 23:35:38 -0300

    resolução

---
+++ c++/aula011/Untitled3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	 int num;
+	 num=90;
+	 
+	 if( (num >= 3 && num <=  8 ) || (num > 9 && num < 15) ){
+	 	cout << "\nValor aceito\n";
+	 }
+	 else{
+	 	cout<<"\nValor recusado\n";
+	 }
+	
+	return 0;
+}
